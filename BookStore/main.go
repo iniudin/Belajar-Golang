124988a00dce0346e4180d666b56c935fd938144
udincrab <sysfdn@gmail.com>
2021-08-05 21:22:41 +0700

    Create REST API using Gin and Gorm

---
+++ BookStore/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+
+	"bookstore/controllers"
+	"bookstore/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	models.ConnectDatabase()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "Server is running!"})
+	})
+
+	r.GET("/books", controllers.FindBooks)
+	r.POST("/books", controllers.CreateBook)
+	r.GET("/books/:id", controllers.FindBook)
+	r.PATCH("/books/:id", controllers.UpdateBook)
+	r.DELETE("/books/:id", controllers.DeleteBook)
+
+	r.Run()
+}
---
+++ BookStore/models/setup.go
@@ -0,0 +1,19 @@
+package models
+
+import (
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDatabase() {
+	database, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+
+	if err != nil {
+		panic("Failed connect to database")
+	}
+
+	database.AutoMigrate(&Book{})
+	DB = database
+}
